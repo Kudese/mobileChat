21e8c9fc593785dd25b7510648b850ba27713f23
Kudese <d.kuprichenkov@gmail.com>
2023-05-25 21:18:21 +0300

    add pages login post

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import { ImageBackground, View } from "react-native/types";
+import imagBG from "./images/photoBG.jpg"
+
+export default function LoginScreen() {
+  return (
+    <View>
+     <ImageBackground source={imagBG} /> 
+      
+    </View>
+  );
+}
