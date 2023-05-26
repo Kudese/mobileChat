52de321a46c46fba9d31d12bf456904c1c4c3a72
Kudese <d.kuprichenkov@gmail.com>
2023-05-26 03:31:52 +0300

    add registrated screen

--- App.js
+++ App.js
@@ -1,19 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Image,
+  ImageBackground,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import imagBG from "./Screens/images/photoBG.jpg";
 export default function App() {
+  // const [fontsLoaded] = useFonts({
+  //   'Inter-Black': require('./assets/fonts/'),
+  // })
+
   return (
     <View style={styles.container}>
-      <Text>Open upd!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={styles.background_images}
+        source={require("./Screens/images/photoBG.jpg")}
+      >
+        <View style={styles.back_container}>
+          <View style={styles.form_container}>
+            <Text style={styles.title}> Реєстрація </Text>
+
+            <TextInput placeholder="Логін" style={styles.loginInput} />
+            <TextInput
+              placeholder="Адреса електронної пошти"
+              style={styles.emailInput}
+            />
+            <TextInput placeholder="Пароль" style={styles.passwordInput} />
+
+            <View style={styles.btncontainer}>
+              <TouchableOpacity activeOpacity={0.7} style={styles.btnSingUp}>
+                <Text style={styles.btnText}>Зареєстуватися</Text>
+              </TouchableOpacity>
+              <Text style={styles.loginScren}>Вже є акаунт? Увійти</Text>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: "#fff",
+  },
+  background_images: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    resizeMode: "cover",
+  },
+  back_container: {
+    marginTop: "auto",
+    justifyContent: "flex-end",
+  },
+  form_container: {
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    alignContent: "center",
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    marginTop: 92,
+    marginLeft: "auto",
+    marginRight: "auto",
+    //TODO
+  },
+  loginInput: {
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+    marginTop: 33,
+    marginHorizontal: 16,
+    padding: 16,
+    height: 50,
+  },
+  emailInput: {
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+    marginTop: 16,
+    marginHorizontal: 16,
+    padding: 16,
+    height: 50,
+  },
+  passwordInput: {
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+    marginTop: 16,
+    marginHorizontal: 16,
+    padding: 16,
+    height: 50,
+  },
+  btnSingUp: {
+    height: 51,
+    marginHorizontal: 16,
+    backgroundColor: "#FF6C00",
+    marginTop: 41,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  btnText: {
+    color: "#FFFFFF",
+  },
+  btncontainer: {
+
+    justifyContent:"center"
+  },
+  loginScren: {
+    flex:1,
+ marginHorizontal: "auto"
   },
 });
--- Screens/AuthSreen/LoginScreen.js
+++ Screens/AuthSreen/LoginScreen.js
@@ -1,10 +1,10 @@
-import { ImageBackground, View } from "react-native/types";
+import {  View } from "react-native/types";
 import imagBG from "./images/photoBG.jpg"
 
 export default function LoginScreen() {
   return (
     <View>
-     <ImageBackground source={imagBG} /> 
+     
       
     </View>
   );
