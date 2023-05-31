be3eff4ecb905d36f0db184df96c31e67e51111b
Kudese <d.kuprichenkov@gmail.com>
2023-06-01 01:32:14 +0300

    add camera

---
+++ Screens/MainScreen/CreatePostsScreen/CreatePostsScreen,style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  conteiner: {
+    flex: 1,
+  },
+  cameraconteiner: {
+    marginHorizontal: 16,
+    borderRadius: 8,
+  },
+  camera: {
+    height: 240,
+    borderRadius: 8,
+  },
+  
+  inputname: {
+    marginTop:48,
+    marginHorizontal: 16,
+    fontFamily: "RobotoMediun",
+    fontSize: 16,
+  },
+});
+export default styles;
--- Screens/MainScreen/CreatePostsScreen/CreatePostsScreen.js
+++ Screens/MainScreen/CreatePostsScreen/CreatePostsScreen.js
@@ -1,10 +1,26 @@
-import { Text } from "react-native"
+import { Text, Touchable, View } from "react-native";
+import { Camera,CameraType } from "expo-camera";
+import styles from "./CreatePostsScreen,style";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { useState } from "react";
+const CreatePostsScreen = () => {
+    const [uriPhoto,setUriPhoto]= useState("")
+    const[name, setName]= useState("")
+    const [photo ,setPhoto]= useState(null)
+   const [location, setLocation]= useState("")
 
- const CreatePostsScreen = ()=>{
+  return (
+    <View style={styles.conteiner} >
+      <View style={styles.cameraconteiner}>
+        <Camera style={styles.camera} type={CameraType.front} ></Camera>
+        <TouchableOpacity onPress={()=>{createPhoto()}} >
+        </TouchableOpacity>
+        <Text>{photo?"Редагувати фото":"Завантажте фото"}</Text>
+      </View>
+        <TextInput style={styles.inputname} placeholder="Назва..." />
+        <TextInput style={styles.inputlocation} placeholder="Місцевість..."/>
+    </View>
+  );
+};
 
-    return(
-        <Text>CreatePostsScreen</Text>
-    )
-}
-
-export default CreatePostsScreen
+export default CreatePostsScreen;
